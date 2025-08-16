6530f2727cd86f8c804ed955f6cbe28956bcbce3
Saheba Kamal <sahebakamal6>
2025-08-16 15:20:24 +0500

    change some code

--- app/api/login/route.js
+++ app/api/login/route.js
@@ -1,19 +1,32 @@
 import { connectDB } from "@/lib/mongodb";
-import Review from "@/models/Review";
+import User from "@/models/User";
+import bcrypt from "bcryptjs";
+import { signToken } from "@/lib/auth";
+import { serialize } from "cookie";
 
 export async function POST(req) {
   try {
     await connectDB();
+    const { email, password } = await req.json();
 
-    const { userId, hijabId, rating, comment } = await req.json();
+    const user = await User.findOne({ email });
+    if (!user) return new Response(JSON.stringify({ error: "Invalid email" }), { status: 400 });
 
-    if (!userId || !hijabId || !rating) {
-      return new Response(JSON.stringify({ error: "Missing required fields" }), { status: 400 });
-    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return new Response(JSON.stringify({ error: "Invalid password" }), { status: 400 });
 
-    const newReview = await Review.create({ userId, hijabId, rating, comment });
+    const token = signToken(user);
 
-    return new Response(JSON.stringify({ message: "Review submitted", review: newReview }), { status: 201 });
+    return new Response(JSON.stringify({ message: "Login success" }), {
+      status: 200,
+      headers: {
+        "Set-Cookie": serialize("token", token, {
+          path: "/",
+          httpOnly: true,
+          maxAge: 60 * 60,
+        }),
+      },
+    });
   } catch (err) {
     return new Response(JSON.stringify({ error: err.message }), { status: 500 });
   }
